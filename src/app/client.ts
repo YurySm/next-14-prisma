b3bd99421faf8f8adb7e33daf66a110c007e035d
Yury Smolko <49496382+YurySm@users.noreply.github.com>
2023-11-04 19:40:06 +0300

    Add files via upload

---
+++ src/app/client.ts
@@ -0,0 +1,8 @@
+import {PrismaClient} from ".prisma/client";
+
+
+export async function getClient() {
+    const client = new PrismaClient()
+    await client.$connect()
+    return client
+}
---
+++ src/app/form/add-todo.tsx
@@ -0,0 +1,11 @@
+'use client'
+import {addAction} from "@/app/form/add-action";
+
+export function AddTodo() {
+    return (
+        <form action={addAction}>
+            <input name={'description'} type={'text'}/>
+            <input type={'submit'}/>
+        </form>
+    )
+}
